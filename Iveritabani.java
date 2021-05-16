d9129c55e32203c4b79f8bf9207d9b3a7f683c86
hemlockk <62615411+hemlockk@users.noreply.github.com>
2021-05-16 21:59:55 +0300

    Delete Iveritabani.java

--- Iveritabani.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.sql.Connection;
-
-public interface Iveritabani {
-    public Connection baglan();
-    public boolean GirisYap(String KullaniciAdi, String Sifre);
-}

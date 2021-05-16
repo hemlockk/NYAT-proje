9492ade709216d815aaada1c722c4f28a205049c
hemlockk <62615411+hemlockk@users.noreply.github.com>
2021-05-16 21:59:46 +0300

    Delete Ikullanici.java

--- Ikullanici.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface Ikullanici {
-    public kullanici setPass(String Pass);
-    public String getPass();
-    public kullanici setName(String isim);
-    public String getName();
-    public kullanici setSogutucuSonDurumu(boolean durum);
-}
